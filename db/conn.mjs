461a2fdee12abfeb6f04672c25964b2b16c690a2
oldirtychi <ejsapayanp@gmail.com>
2024-10-02 22:25:44 -0500

    cleanup

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,19 +1,3 @@
-// import { MongoClient } from "mongodb";
-
-// const connectionString = process.env.ATLAS_URI || "";
-
-// const client = new MongoClient(connectionString);
-
-// let conn;
-// try {
-//     conn = await client.connect();
-// }   catch (e) {
-//     console.error(e);
-// }
-
-// let db = conn.db("");
-
-// export default db;
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
--- models/index.mjs
+++
@@ -1,3 +0,0 @@
-// export { default as User } from './User.js';
-// export { default as Post } from './Post.js';
-// export { default as Comment } from './Comment.js';
